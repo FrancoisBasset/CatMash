936c62ae8100bda9ebf8d194decdcc46ba03ac08
francois <francois@mail.fr>
2020-03-13 23:00:16 +0100

    Test : Recuperation JSON
    Suppression de test_cats_json.js

--- test_cats_json.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-fetch('https://latelier.co/data/cats.json').then(function(res) {
-	res.json().then(function(json) {
-		for (const image of Object.values(json.images)) {
-			console.log(`ID : ${image.id}, URL: ${image.url}`);
-		}
-	});
-});
