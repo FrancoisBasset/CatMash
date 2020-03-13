3ce32622f73b650ef5d11a1a1ba1c9b0e3aead54
francois <francois@mail.fr>
2020-03-13 09:19:35 +0100

    Node.Js: Partie web
    Integration d'Express
    Creation des routes / et /classement

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-process.stdout.write('It works !');
+const express = require('express');
+const app = express();
+
+app.listen(80, function() {
+	console.log('Listen on 80');
+});
+
+app.get('/', function(req, res) {
+	res.send('Vote !');
+});
+
+app.get('/classement', function(req, res) {
+	res.send('Classement !');
+});
